8933c5b06e22ea197056a47ca320ddd5d6a3c740
vaibhavmallya98 <45683079+vaibhavmallya98@users.noreply.github.com>
2024-05-21 23:56:12 -0400

    Add files via upload

---
+++ langchain_helper.py
@@ -0,0 +1,63 @@
+from langchain.vectorstores import FAISS
+from langchain.llms import GooglePalm
+from langchain.document_loaders.csv_loader import CSVLoader
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.prompts import PromptTemplate
+from langchain.chains import RetrievalQA
+import os
+
+from dotenv import load_dotenv
+load_dotenv()  # take environment variables from .env (especially openai api key)
+
+# Create Google Palm LLM model
+llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.5)
+# # Initialize instructor embeddings using the Hugging Face model
+instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
+vectordb_file_path = "faiss_index"
+
+def create_vector_db():
+    # Load data from FAQ sheet
+    loader = CSVLoader(file_path='GeorgiaTech_Admission_FAQs.csv', source_column="prompt")
+    data = loader.load()
+
+    # Create a FAISS instance for vector database from 'data'
+    vectordb = FAISS.from_documents(documents=data,
+                                    embedding=instructor_embeddings)
+
+    # Save vector database locally
+    vectordb.save_local(vectordb_file_path)
+
+
+def get_qa_chain():
+    # Load the vector database from the local folder
+    vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
+
+    # Create a retriever for querying the vector database
+    retriever = vectordb.as_retriever(score_threshold=0.7)
+
+    prompt_template = """Given the following context and a question, generate an answer based on this context only.
+    Give a concise answer from the "response" section in the source document context.
+    If the question asked is a combination of multiple questions from the source document, combine the answers for those questions and display as a single answer. 
+    If the answer is not found in the context, kindly state "Sorry, I don't know." Don't create your own answers.
+
+    CONTEXT: {context}
+
+    QUESTION: {question}"""
+
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context", "question"]
+    )
+
+    chain = RetrievalQA.from_chain_type(llm=llm,
+                                        chain_type="stuff",
+                                        retriever=retriever,
+                                        input_key="query",
+                                        return_source_documents=True,
+                                        chain_type_kwargs={"prompt": PROMPT})
+
+    return chain
+
+if __name__ == "__main__":
+    create_vector_db()
+    chain = get_qa_chain()
+    print(chain("Do you have javascript course?"))
